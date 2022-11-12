795c099d0c19d8ef2789758528bb4f0c6ff65375
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-11-12 22:04:18 +0530

    Added: UC3 Player win or lose

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,27 +1,36 @@
 package com.bridgelabz;
-import java.util.Random;
+
 
 public class GamblingSimulation {
-    public static final int STAKE=100;
-    public static final int BET=1;
+    public static final int STAKE = 100;
+    public static final int BET = 1;
+    static int stake=100;
+
 
     //check win or lose
-    public void winOrLose(){
-        Random random=new Random();
-        int value= random.nextInt(2);
+    public void winOrLose() {
+        int value=(int)(Math.random()*2);
 
-        if (value==1){
-            System.out.println("player Win");
-        }else
-            System.out.println("player Lose");
+        while(stake > 50 && stake < 150){
+
+            if (value==1) {
+                stake++;
+                System.out.println("player Win stake is:" + stake);
+            }else{
+                stake--;
+                    System.out.println("player Lose stake is:" +stake);}
+
+            }
 
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to gambling Simulation program");
-        System.out.println("You have Stake every day = " +(STAKE));
-        System.out.println("you bet every game = " +(BET));
+        System.out.println("You have Stake every day = " + (STAKE));
+        System.out.println("you bet every game = " + (BET));
 
-        GamblingSimulation simulator= new GamblingSimulation();
+        GamblingSimulation simulator = new GamblingSimulation();
         simulator.winOrLose();
     }
 }
+
