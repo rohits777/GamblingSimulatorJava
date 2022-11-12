fdb413142d68576f88be4f5b7905d53de5f343ab
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-11-12 20:38:11 +0530

    Added: UC1 bet and stake

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,7 +1,11 @@
 package com.bridgelabz;
 
 public class GamblingSimulation {
+    public static final int STAKE=100;
+    public static final int BET=1;
     public static void main(String[] args) {
         System.out.println("Welcome to gambling Simulation program");
+        System.out.println("You have Stake every day = " +(STAKE));
+        System.out.println("you bet every game = " +(BET));
     }
 }
